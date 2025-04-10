451c6d758cc3a1d758f0da4aab2697bfcad283fa
arslanevren <earslan3e@gmail.com>
2025-04-10 11:04:01 +0200

    dockerfile updated

--- 01-docker-terraform/2_docker_sql/ingest_data.py
+++ 01-docker-terraform/2_docker_sql/ingest_data.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
-
 import os
 import argparse
-
 from time import time
-
 import pandas as pd
 from sqlalchemy import create_engine
 
@@ -31,24 +28,18 @@ def main(params):
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
 
     df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
-
     df = next(df_iter)
-
     df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
     df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
 
     df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
-
     df.to_sql(name=table_name, con=engine, if_exists='append')
 
-
     while True: 
-
         try:
             t_start = time()
-            
-            df = next(df_iter)
 
+            df = next(df_iter)
             df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
             df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
 
@@ -64,7 +55,6 @@ def main(params):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
-
     parser.add_argument('--user', required=True, help='user name for postgres')
     parser.add_argument('--password', required=True, help='password for postgres')
     parser.add_argument('--host', required=True, help='host for postgres')
--- 2_docker_sql/pipeline.py
+++
@@ -1,8 +0,0 @@
-import sys
-import pandas as pd
-
-print(sys.argv)
-
-day = sys.argv[1]
-
-print(f"Pipeline started for day: {day}")
